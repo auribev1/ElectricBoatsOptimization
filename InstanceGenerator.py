91d8183856e523f0ad38796999c9f493249ef121
auribev1 <auribev1@eafit.edu.co>
2023-06-05 16:11:06 -0500

    Organizar la manera de seleccionar si se eligen los nodos de derecha o izquierda de la instancia siendo -1 izquierda y 1 derecha. Se corrigio el calculo de la matriz de distancias. La velocidad del rio se paso a km/h. Se quito la escritura en el txt. Se corrigio el calculo del tiempo de recorrido. Por ultimo se corrigio todo para almacenar en un unico json

--- InstanceGenerator.py
+++ InstanceGenerator.py
@@ -20,24 +20,33 @@ nodes.dist = nodes.dist.astype(float)
 n = int(info[vars.index('n')])  # Numero de nodos a utilizar
 
 # Filtrar el numero de nodos seleccionado por el usuario
-center = nodes.loc[nodes.pos == 0].index[0]
-upper_limit = min(center + n, len(nodes))
-lower_limit = max(0, center - n)
-N = nodes.iloc[lower_limit:upper_limit+1].reset_index(drop=True)    # Lista de nodos
+pos = int(info[vars.index('pos')])
+center = nodes.loc[nodes.node == 'Puerto Calvo'].index[0]   # Nodo del deposito
+if pos == -1:
+    limit = center - n
+    N = nodes.iloc[limit:center+1].sort_values(by='pos', ascending=False).reset_index(drop=True)
+else:
+    limit = center + n
+    N = nodes.iloc[center:limit+1].reset_index(drop=True)
 
 # Estimar la información de distancias
 dist_info = np.zeros((len(N), len(N)))
+
 for i in range(len(N)):
     for j in range(len(N)):
-        N_aux = N.iloc[min(i, j):max(i, j)]
-        dist_info[i][j] = N_aux.dist.sum()
+        if i != j:
+            N_aux = N.iloc[min(i, j)+1:max(i, j)+1]
+            dist_info[i][j] = N_aux.dist.sum()
+        else:
+            dist_info[i][j] = 0
+
 
 # Velocidades
 v_min = float(info[vars.index('v_min')])  # km/h
 v_max = float(info[vars.index('v_max')])  # km/h
 v_step = float(info[vars.index('v_step')])  # km/h
 V = np.linspace(v_min, v_max, num=int((v_max-v_min)/v_step)+1, endpoint=True, dtype=int)
-v_river = float(info[vars.index('v_river')])  # metros por segundo
+v_river = float(info[vars.index('v_river')])  # km/h
 
 # Capacidad de carga del bote en kg
 Q = float(info[vars.index('Q')])
@@ -47,14 +56,9 @@ Q_min = float(info[vars.index('Q_min')])
 Q_step = float(info[vars.index('Q_step')])
 W = np.linspace(Q_min, Q, num=int((Q-Q_min)/Q_step)+1, endpoint=True, dtype=int) # Se debe convertir a m/s
 
-# Aqui crear el json con la información de las distancias
+# Aqui crear el diccionario con la información de las distancias
 boat = Boat()
-instance = []
-
-# Crea los labels de la tabla de instancias
-text = open("instance.txt", "a")
-text.write(f"origin;destiny;v;w;d;t;e;flow\n")
-text.close()
+matrix = []
 
 for v in V:
     for c in W:
@@ -62,13 +66,13 @@ for v in V:
             for j in range(len(dist_info)):
                 dist = dist_info[i][j] * 1000
                 if N.iloc[i].pos > N.iloc[j].pos:
-                    vel = (v / 3.6)   # pasarlo a metros por segundo
+                    vel = (v - v_river) / 3.6  # pasarlo a metros por segundo, va a favor de la corriente
                     current = False
                 else:
-                    vel = (v / 3.6) + v_river   # Si va en contra corriente se le suma la velocidad del rio
+                    vel = (v + v_river) / 3.6   # Si va en contra corriente se le suma la velocidad del río
                     current = True
                 w = c
-                t = dist / vel  # Se calcula el tiempo de viaje
+                t = dist / (v / 3.6)  # Se calcula el tiempo de viaje con respecto a la velocidad determinada
                 inst_pow = boat.drag_sav(vel, w)    # Se calcula la potencia instantanea con modelo del bote
                 e = inst_pow * t / 3600     # Se calcula el consumo
                 arc_info = {
@@ -76,19 +80,12 @@ for v in V:
                     "destiny": int(j),
                     "v": float(v),
                     "w": float(c),
-                    "d": float(dist),
+                    "dist": float(dist),
                     "t": float(t),
                     "e": float(e),
                     "flow": current
                 }
-                instance.append(arc_info)
-                text = open("instance.txt", "a")
-                text.write(f"{int(i)};{int(j)};{float(v)};{float(c)};{float(dist)};{float(t)};{float(e)};{current}\n")
-                text.close()
-
-# Guarda en formato json
-with open('instance.json', 'w') as archivo:
-    json.dump(instance, archivo)
+                matrix.append(arc_info)
 
 # Demanda en peso de cada nodo en orden
 d = np.random.choice(W, size=n, replace=True)
@@ -113,13 +110,19 @@ farthest_place = dist_info[0][-1]   # Lugar más lejano en km
 mean_t = farthest_place * 1000 / (mean_v / 3.6)     # tiempo en segundos hacia el lugar más lejano
 l = np.random.uniform(low=service_lower_lim, high=service_upp_lim, size=n) * mean_t
 
-# Escribe el archivo con vectores adicionales
-with open('instance2.txt', 'w') as archivo:
-    archivo.write('W' + ':' + ','.join(map(str, W)) + '\n')
-    archivo.write('V' + ':' + ','.join(map(str, V)) + '\n')
-    archivo.write('d' + ':' + ','.join(map(str, d)) + '\n')
-    archivo.write('E' + ':' + str(E) + '\n')
-    archivo.write('Q' + ':' + str(Q) + '\n')
-    archivo.write('f' + ':' + str(f) + '\n')
-    archivo.write('s' + ':' + str(s) + '\n')
-    archivo.write('T_min' + ':' + str(T_min) + '\n')
+# Guarda en formato json
+instance = {
+    "matriz": matrix,
+    "W": W.tolist(),
+    "V": V.tolist(),
+    "d": d.tolist(),
+    "E": E,
+    "Q": Q,
+    "f": f,
+    "s": s,
+    "T_min": T_min,
+    "l": l.tolist(),
+}
+
+with open('instance.json', 'w') as archivo:
+    json.dump(instance, archivo)
